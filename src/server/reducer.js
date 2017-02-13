831c2137fce71e4a9241e2b13f37dbfa700e01b4
tiger <724475200@qq.com>
2017-02-13 08:42:49 +0800

    reducer

--- src/server/reducer.js
+++ src/server/reducer.js
@@ -1,3 +1,20 @@
 /**
  * Created by zhoujialin on 2017/2/10.
  */
+import {
+  addRoom,
+  removeRoom
+} from "./core.js"
+
+//分配事件处理
+export default function reducer(state, action){
+  switch(action.type){
+    case "ADD_ROOM":
+      return addRoom(state, action.room)
+    case "REMOVE_ROOM":
+      return removeRoom(state, action.payload)
+  }
+
+  return state
+}
+
--- test/server/reducer_spec.js
+++ test/server/reducer_spec.js
@@ -7,7 +7,21 @@ import coreReducer from "../../src/server/reducer"
 describe("server端核心Reducer", () => {
 
   it("可以当做一个reducer", () => {
+    var id = v1()
+    //定义动作数据
+    var actions = [
+      {type:"ADD_ROOM",room:{id,name:"1",owner:"eisneim"}},
+      {type:"ADD_ROOM",room:{name:"2",owner:"terry"}},
+      {type:"ADD_ROOM",room:{name:"3",owner:"eisneim"}},
+      {type:"REMOVE_ROOM",payload:{id:id,user:"eisneim"}},
+    ]
+    const finalState = actions.reduce(coreReducer, undefined)
+    console.log(finalState)
 
+    //检查，最后剩下的房间数量是否是2个
+    expect(finalState.get("rooms").size).to.equal(2)
+    //检查，第一个房间所有者是谁
+    expect(finalState.getIn(["rooms",0,"owner"])).to.equal("terry")
   })
 
 
